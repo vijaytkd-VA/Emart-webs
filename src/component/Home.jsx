ea265a4c0e14fc1e53c7e4aabdeb58cd5a3d22fa
vijaytkd-VA <mohitevijaysinh8379@gmail.com>
2023-08-06 16:42:18 +0530

    new

---
+++ src/component/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Products from "./Products";
+
+const Home = () => {
+  return (
+    <div className="hero">
+      <div className="card bg-dark text-white border-0">
+        <div class="container">
+        <img src="/assets/bg.jpg" className="card-img" alt="Background" height="700px" style={{ objectFit: "cover",objectPosition: "20% right",width: "100%",}}/></div>
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
+            <div className="container">
+          <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
+          <p className="card-text lead fs-2">
+            CHECK OUT ALL THE TRENDS!
+          </p>
+            </div>
+        </div>
+      </div>
+      <Products/>
+    </div>
+  );
+};
+
+export default Home;
